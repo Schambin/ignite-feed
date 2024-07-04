cd9d996db6a3163ad37cecaa9bcfee4030cff5dc
Kauan <raidoka546@gmail.com>
2024-07-04 08:51:25 -0400

    user posts part

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,31 +1,40 @@
 import style from './Post.module.css';
 
-export function Post(props) {
+export function Post() {
     return (
         <article className={style.post}>
             <header>
                 <div className={style.author}>
-                    <img src="" alt="" />
-                    <span>Jane Cooper</span>
-                    <p>Dev Front-End</p>
+                    <img 
+                        src="https://github.com/Schambin.png" 
+                        alt="" 
+                        className={style.authorImage}
+                    />
+                    <div className={style.authorData}>
+                        <span>Jane Cooper</span>
+                        <p>Dev Front-End</p>    
+                    </div>
                 </div>
 
-                <span>Publicado à 1h</span>
+                    <time title='03 de Julho às 09:04h' dateTime="">Publicado à 1h</time>
             </header>
 
-            <main>
-                <div>
-                    Fala galeraa 👋
-                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                    👉 jane.design/doctorcare
-                    #novoprojeto #nlw #rocketseat
-                </div>
-            </main>
+            <div className={style.postContent}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+                👉
+                </p>
+                <p><a href="#">jane.design/doctorcare</a></p>
+                <p><a href="#">#novoprojeto</a></p><p><a href="#">#nlw</a></p> <p><a href="#">#rocketseat</a></p>
+            </div>
 
             <footer>
-                <span>Deixe seu feedback</span>
-                <input type="text" />
-                <button>Publicar</button>
+                <div className={style.bottomContent}>
+                    <div className={style.border} />
+                    <span>Deixe seu feedback</span>
+                    <textarea />
+                    <button>Publicar</button>
+                </div>
             </footer>
         </article>
     )
