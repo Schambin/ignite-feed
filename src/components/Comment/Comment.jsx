47c2583516d6e10fc3057aa8808c223a73322392
Kauan <raidoka546@gmail.com>
2024-07-04 18:00:25 -0400

    comment section aligned

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -4,28 +4,28 @@ import styles from './Comment.module.css';
 
 export function Comment() {
     return (
-        <section className={styles.comment}>
+        <div className={styles.commentSection}>
             <img src="https://github.com/Schambin.png" />
-
-            <header className={styles.commentBox}>
-                <div className={styles.userData}>
-
-                    <strong>John Doe (você)</strong> 
-                    <time title="03 de Julho às 09:04h" datetime="">Cerca de 2h</time>
-
-                </div>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna sed vulputate ullamcorper, nunc velit fringilla felis, in consectetur risus felis vel diam.</p>
-                <button title="Deletar">
-                    <PiTrash size={20}/>
-                </button>
-            </header>
-            <footer className={styles.applausePart}>
-                <button title="Aplaudir">
-                    <PiThumbsUpBold />
-                </button>
-                Aplaudir <span>20</span>
-            </footer>
-        </section>
+            <div>
+                <header className={styles.commentBox}>
+                    <div className={styles.commentBoxUser}>
+                        <div className={styles.userData}>
+                            <strong>John Doe (você)</strong>
+                            <time title="03 de Julho às 09:04h">Cerca de 2h</time>
+                        </div>
+                            <button title="Deletar">
+                                <PiTrash size={24}/>
+                            </button>
+                    </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna sed vulputate ullamcorper, nunc velit fringilla felis, in consectetur risus felis vel diam.</p>
+                </header>
+                <footer className={styles.applauseSection}>
+                    <button title="Aplaudir">
+                        <PiThumbsUpBold />
+                        Aplaudir <span>20</span>
+                    </button> 
+                </footer>
+            </div>
+        </div>
     )
 }
