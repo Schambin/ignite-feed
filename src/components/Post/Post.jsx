d44fc17f175d090bb0cdb84d6ddb8623813dc0c8
Kauan <raidoka546@gmail.com>
2024-07-08 16:51:41 -0400

    dates, avatar and author information comes from the 'App.jsx' simulation

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,31 +1,49 @@
+import style from './Post.module.css';
+import ptBR from 'date-fns/locale/pt-BR';
+
+
+import { format, formatDistanceToNow } from 'date-fns';
 import { Avatar } from '../Avatar/Avatar';
 import { Comment } from '../Comment/Comment'
 
-import style from './Post.module.css';
 
-export function Post() {
+
+export function Post(properties) {
+    const publishedDate = format(properties.publishedAt, "dd 'de' LLLL 'às' HH:mm'h' ", {
+        locale: ptBR,
+    })
+
+    
+    const publishedDateRelativeToNow = formatDistanceToNow(properties.publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    } )
+
     return (
         <article className={style.post}>
             <header>
                 <div className={style.author}>
                     
-                       <Avatar src="https://github.com/Schambin.png"/>
+                       <Avatar src={properties.author.avatarUrl}/>
                     <div className={style.authorData}>
-                        <span>Jane Cooper</span>
-                        <p>Dev Front-End</p>    
+                        <span>{properties.author.name}</span>
+                        <p>{properties.author.role}</p>    
                     </div>
                 </div>
 
-                    <time title='03 de Julho às 09:04h' dateTime="">Publicado à 1h</time>
+                    <time title={publishedDate} dateTime={properties.publishedAt.toISOString()}>
+                        {publishedDateRelativeToNow}
+                    </time>
             </header>
 
             <div className={style.postContent}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                👉
-                </p>
-                <p><a href="#">jane.design/doctorcare</a></p>
-                <p><a href="#">#novoprojeto</a></p><p><a href="#">#nlw</a></p> <p><a href="#">#rocketseat</a></p>
+                {properties.content.map( line => {
+                    if( line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if ( line.type === 'link' ) {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={style.commentForm}>
