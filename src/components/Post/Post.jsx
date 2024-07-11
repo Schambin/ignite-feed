37aac7b50795d7fc43433eb9691eaac504fb6930
Kauan <raidoka546@gmail.com>
2024-07-11 10:17:58 -0400

    adding keys

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -58,9 +58,9 @@ export function Post(properties) {
             <div className={style.postContent}>
                 {properties.content.map( line => {
                     if( line.type === 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if ( line.type === 'link' ) {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
